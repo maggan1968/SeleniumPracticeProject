dd0edf97355801efa7c29302ee05a3dbb9f9bbfd
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2021-09-13 14:39:46 -0500

    day5 :Added JUnit 5 First Junit Test with 2 tasts

--- src/test/java/com/cybertek/tests/day5_css_xpath_junit5/FirstTest.java
+++ src/test/java/com/cybertek/tests/day5_css_xpath_junit5/FirstTest.java
@@ -3,8 +3,16 @@ package com.cybertek.tests.day5_css_xpath_junit5;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+/**
+ * package always LowerCase
+ * class name always UpperCase
+ */
+
 public class FirstTest {
 
+
+    //Test method is annotated with @Test annotation
+    //Its a void method with no parameter
     @Test
     public void testAddition(){
 
@@ -13,7 +21,23 @@ public class FirstTest {
         int expectedResult = 17 ;
         Assertions.assertEquals(expectedResult , num1+num2);
 
+
+
     }
+    @Test
+    public void testString(){
+
+        String msg = "I love Selenium";
+        //assert this msg equal "I love Selenium"
+        //below assertEquals method accept 2parameter
+        Assertions.assertEquals("I love Selenium" , msg);
+
+       // assert this msg starts with " I love"
+      // below assertTrue method accept 1 parameter as boolean
+        Assertions.assertTrue(  msg.startsWith("I love"));
+
+    }
+
 
 }
 
