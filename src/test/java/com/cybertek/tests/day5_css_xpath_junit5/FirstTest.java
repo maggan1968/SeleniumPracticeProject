293cebae0750e173ba351b00dcb79ab2a538f444
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2021-09-13 14:18:21 -0500

    day5 :Added JUnit 5 First Junit Test for adding 2 numbers

--- src/test/java/com/cybertek/tests/day5_css_xpath_junit5/FirstTest.java
+++ src/test/java/com/cybertek/tests/day5_css_xpath_junit5/FirstTest.java
@@ -1,9 +1,21 @@
 package com.cybertek.tests.day5_css_xpath_junit5;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class FirstTest {
 
     @Test
-    public void testAddition(){}
+    public void testAddition(){
+
+        int num1 = 10 ;
+        int num2 = 7 ;
+        int expectedResult = 17 ;
+        Assertions.assertEquals(expectedResult , num1+num2);
+
+    }
+
 }
+
+
+
