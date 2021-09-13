cd8c670b588a0bde04f17b46f94f1943d4e07e13
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2021-09-13 15:25:38 -0500

    day5 :Added JUnit 5 Yahoo Search Result Page Title Test

--- src/test/java/com/cybertek/tests/day5_css_xpath_junit5/YahooSearchPageTest.java
+++ src/test/java/com/cybertek/tests/day5_css_xpath_junit5/YahooSearchPageTest.java
@@ -2,10 +2,14 @@ package com.cybertek.tests.day5_css_xpath_junit5;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class YahooSearchPageTest {
 
@@ -43,12 +47,40 @@ public class YahooSearchPageTest {
 
     }
 
-
-    }
     // test method name : testYahooSearchResultPageTitle
     // test navigate to yahoo page
     // and search for Selenium
     // the page title should start with selenium
+
+
+    @Test
+    public void testYahooSearchResultPageTitle(){
+
+        WebDriverManager.chromedriver().setup();
+        //create chromeDriver object
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://search.yahoo.com/");
+
+        //identify searchBox end enter selenium, and hit Enter key on keyboard
+        WebElement searchBox = driver.findElement(By.xpath("//input[@name='p']"));
+        // we can simulate keystroke usinh Keys.SELECT_ONE_OF_THE_OPTION
+        // In this case we are typing selenium and hitter enter
+        searchBox.sendKeys("Selenium" + Keys.ENTER);
+
+       // String expectedTitleStartWith = "selenium";
+        String actualTitle = driver.getTitle();
+
+        driver.quit();
+        //assert the title start with Selenium
+
+       assertTrue(actualTitle.startsWith("Selenium"));
+    }
+
+
+    }
+
+
 
 
 
