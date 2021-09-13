56c477d50817491f75c29317e36826cb49bddc85
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2021-09-13 15:11:28 -0500

    day5 :Added JUnit 5 First Junit Test with 2 tasts

---
+++ src/test/java/com/cybertek/tests/day5_css_xpath_junit5/YahooSearchPageTest.java
@@ -0,0 +1,54 @@
+package com.cybertek.tests.day5_css_xpath_junit5;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class YahooSearchPageTest {
+
+    // write 2 tests :
+    // testYahooSearchHomePageTitle
+    //
+    //    test when you navigate to yahoo search page
+    //        the title should be "Yahoo Search - Web Search"
+
+
+    @Test
+    public void testYahooSearchHomePageTitle() {
+
+    //set up chrome driver
+    WebDriverManager.chromedriver().setup();
+
+    //create chromeDriver object
+    WebDriver driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.get("https://search.yahoo.com/");
+
+        String expectedTitle = "Yahoo Search - Web Search" ;
+        String actualTitle = driver.getTitle();
+
+        // quit the browser here , because we already saved the title
+        driver.quit();
+        // do static import, so you can do this
+        // import static org.junit.jupiter.api.Assertions.*;
+        assertEquals(expectedTitle,actualTitle ) ;
+
+
+
+
+
+
+    }
+
+
+    }
+    // test method name : testYahooSearchResultPageTitle
+    // test navigate to yahoo page
+    // and search for Selenium
+    // the page title should start with selenium
+
+
+
